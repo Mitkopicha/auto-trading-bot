9ff1dcf6c637e34de7ec12aad45343ec9b969c55
Dimitar Dutchev <dimitar.dutchev@gmail.com>
2026-01-20 01:03:00 +0000

    Update seed account data

--- db/seeds/001_seed_account.sql
+++ db/seeds/001_seed_account.sql
@@ -1,5 +1,6 @@
 -- db/seeds/001_seed_account.sql
 -- Initial account seed for trading bot
 
-INSERT INTO account (cash_balance)
-VALUES (1000.00000000);
+INSERT INTO account (cash_balance) VALUES (10000.00000000); -- TRADING (accountId = 1)
+INSERT INTO account (cash_balance) VALUES (10000.00000000); -- TRAINING (accountId = 2)
+
